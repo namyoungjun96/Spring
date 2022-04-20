3d5f5c968fcef982d7d6eb813bab7859f233b700
namyoungjun96 <ehdgornltls@naver.com>
2022-04-20 22:24:43 +0900

    2022.04.20 spring study

--- Toby_Spring/src/main/java/springbook/user/dao/DaoFactory.java
+++ Toby_Spring/src/main/java/springbook/user/dao/DaoFactory.java
@@ -1,15 +1,30 @@
 package springbook.user.dao;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 @Configuration
 public class DaoFactory {
 	
 	@Bean
+	public DataSource dataSource() {
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		
+		dataSource.setDriverClass(org.mariadb.jdbc.Driver.class);
+		dataSource.setUrl("jdbc:mysql://localhost/tobi");
+		dataSource.setUsername("root");
+		dataSource.setPassword("1234");
+		
+		return dataSource;
+	}
+	
+	@Bean
 	public UserDao userDao() {
-		ConnectionMaker connectionMaker = new DConnectionMaker();
-		UserDao userDao = new UserDao(connectionMaker);
+		UserDao userDao = new UserDao();
+		userDao.setDataSource(dataSource());
 		return userDao;
 	}
 	
--- Toby_Spring/src/main/java/springbook/user/domain/UserDaoTest.java
+++ Toby_Spring/src/main/java/springbook/user/domain/UserDaoTest.java
@@ -4,13 +4,16 @@ import java.sql.SQLException;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 import springbook.user.dao.DaoFactory;
 import springbook.user.dao.UserDao;
 
 public class UserDaoTest {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		//ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 		UserDao dao = context.getBean("userDao", UserDao.class);
 		
 		User user = new User();
