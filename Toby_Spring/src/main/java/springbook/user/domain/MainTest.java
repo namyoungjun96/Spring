974e10018561bc95a245614c3ce5b89e6ab2b613
namyoungjun96 <ehdgornltls@naver.com>
2022-04-26 19:47:25 +0900

    2022.04.26 toby spring study

---
+++ Toby_Spring/src/main/java/springbook/user/domain/MainTest.java
@@ -0,0 +1,11 @@
+package springbook.user.domain;
+
+import org.junit.runner.JUnitCore;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		JUnitCore.main("springbook.user.domain.UserDaoTest");
+	}
+
+}
--- Toby_Spring/src/main/java/springbook/user/domain/UserDaoTest.java
+++ Toby_Spring/src/main/java/springbook/user/domain/UserDaoTest.java
@@ -1,35 +1,93 @@
 package springbook.user.domain;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
 import java.sql.SQLException;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
+import javax.sql.DataSource;
 
-import springbook.user.dao.DaoFactory;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import springbook.user.dao.UserDao;
 
-public class UserDaoTest {
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		//ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-		UserDao dao = context.getBean("userDao", UserDao.class);
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="/test-applicationContext.xml")
+public class UserDaoTest {
+	@Autowired
+	private ApplicationContext context;
+	@Autowired
+	private UserDao dao;
+	private User user;
+	private User user2;
+	private User user3;
+	
+	@Before
+	public void setUp() {
+		this.dao = context.getBean("userDao", UserDao.class);
+		
+		this.user = new User("ehdgornltls", "남영준", "1234");
+		this.user2 = new User("fuswns96", "나명준", "1234");
+		this.user3 = new User("dudwns", "남앵준", "1234");
+		
+		System.out.println(this.context);
+		System.out.println(this);
+		
+		DataSource dataSource = new SingleConnectionDataSource("jdbc:mysql://localhost/tobi", "root", "1234", true);
+		dao.setDataSource(dataSource);
+	}
+	
+	@Test
+	public void addAndGet() throws SQLException, ClassNotFoundException {
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
 		
 		User user = new User();
 		user.setId("ehdgornltls");
 		user.setName("남영준");
 		user.setPassword("1234");
-		
+
 		dao.add(user);
-		
-		System.out.println(user.getId() + " 등록 성공");
+		assertThat(dao.getCount(), is(1));
 		
 		User user2 = dao.get(user.getId());
-		System.out.println(user2.getName());
-		System.out.println(user2.getPassword());
 		
-		System.out.println(user2.getId() + " 조회 성공");
+		assertThat(user2.getName(), is(user.getName()));
+		assertThat(user2.getPassword(), is(user.getPassword()));
+	}
+	
+	@Test
+	public void count() throws SQLException, ClassNotFoundException {
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		dao.add(user);
+		assertThat(dao.getCount(), is(1));
+		
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
+		
+		dao.add(user3);
+		assertThat(dao.getCount(), is(3));
+	}
+	
+	@Test(expected=EmptyResultDataAccessException.class)
+	public void getUserFailure() throws SQLException, ClassNotFoundException {
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		dao.get("unkown_id");
 	}
 
 }
